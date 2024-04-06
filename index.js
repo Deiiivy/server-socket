4e413518f00cce3cc59bfc8912c0fa8f5903a4cc
Deiiivy <deivypr28@gmail.com>
2024-04-06 08:41:37 -0500

    initializing project

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const http = require('http');
+const server = http.createServer();
+
+const io = require('socket.io')(server, {
+    cors: { origin: '*' }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Se ha conectado un cliente');
+
+    socket.broadcast.emit('chat_message', {
+        usuario: 'INFO',
+        mensaje: 'Se ha conectado un nuevo usuario'
+    });
+
+    socket.on('chat_message', (data) => {
+        io.emit('chat_message', data);
+    });
+});
+
+
+server.listen(process.env.PORT);
